82ef894498b0fa5a670e8d33af45264e975ed40a
Daniel Santander <danyvo1d@gmail.com>
2020-04-20 17:44:24 -0500

    Finished tagging

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,20 @@
+class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+    # redirect_to tags_path(@tags)
+    redirect_to articles_path(@articles)
+  end
+end
